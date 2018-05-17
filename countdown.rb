5de81762784476b908ed8c40120ea0efe1ee8eb9
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-17 01:24:27 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
-
-def countdown
+def countdown_with_sleep(num)
+  sleep num
 end
+
+def countdown(int)
+  while int != 0 
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR!"
+end
+
